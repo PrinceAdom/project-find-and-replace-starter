bbda469df713d76275de2380b3df678430490269
Prince Adom <jonatan_prog@hotmail.com>
2020-06-08 22:27:18 -0400

    first commit

--- main.js
+++ main.js
@@ -1,38 +1,57 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
-
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
+const replaceOneButton = document.querySelector(".replace-one-button")
+const caseSensitiveSearch = document.querySelector(".case-sensitive")
 
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
+//returns all the 27 rows in the table as an array of of objects
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+//returns all the 7 cells inside a target row as an array of objects
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
+replaceOneButton.addEventListener("click", function(){
 
-// YOUR CODE GOES HERE
+})
 
+replaceAllButton.addEventListener("click", function(){
+    let stringToFind = findInput.value;
+    let stringToReplace = replaceInput.value;
+    let stringRepalcementCount = 0
 
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+    //nested loop traverses each cell
+    for(let row = 0; row < rowElements.length; row++){
+        for(let cell = 0; cell < getCellElements(rowElements[row]).length; cell++){
+            if(getCellElements(rowElements[row])[cell].innerText.includes(stringToFind)){
+                console.log("\"" + stringToFind + "\" found at " + "row " + (row+1) + " cell " + (cell+1))
+                console.log("Cell content: \"" + getCellElements(rowElements[row])[cell].innerText + "\"")
+                console.log("\n")
+
+                //find and replace feature is done here
+                getCellElements(rowElements[row])[cell].innerText = stringMutator(getCellElements(rowElements[row])[cell].innerText, stringToFind, stringToReplace)
+
+                ++stringRepalcementCount
+
+            }
+            else{
+                console.log(`${stringToFind}" not found!`)
+            }
+        }
+
+    }
+
+    setTimeout(function(){
+        alert("number of strings replaced: " + stringRepalcementCount)
+    }, 1000)
+
+})
+
+//method to "mutate" strings
+function stringMutator(stringToMutate, strToFind, strToReplace){
+    let newString = ""
+    newString = stringToMutate.replace(strToFind, strToReplace)
+    return newString
+
+}
